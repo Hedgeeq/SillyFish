9c00189a519bf206b5af7637df3d659039e72d88
eq <abigailmiddleton2008@gmail.com>
2025-07-28 12:52:06 -0400

    Update sillyfish.js

--- sillyfish.js
+++ sillyfish.js
@@ -1,12 +1,3 @@
-const hero = document.querySelector(".center-wrapper");
-const siteContent = document.getElementById("site-content");
-const fishButton = document.getElementById("image-button");
-
-fishButton.addEventListener("click", () => {
-  hero.classList.add("move-up");
-  siteContent.classList.add("reveal");
-  fishButton.disabled = true; // prevent more clicks if needed
-});
 document.addEventListener('DOMContentLoaded', function() {
   const hero = document.querySelector(".center-wrapper");
   const siteContent = document.getElementById("site-content");
