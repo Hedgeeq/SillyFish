43cc7c8d92c20e2e61018d54a731557ae05562e9
eq <abigailmiddleton2008@gmail.com>
2025-07-28 11:58:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const hero = document.getElementById("hero");
+const siteContent = document.getElementById("site-content");
+const fishButton = document.getElementById("image-button");
+
+fishButton.addEventListener("click", () => {
+  hero.classList.add("move-up");
+  siteContent.classList.add("reveal");
+  fishButton.disabled = true; // prevent more clicks if needed
+});
 
