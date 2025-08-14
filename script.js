6202ccba59f2aae77dc264cb9992e1629bc4730b
eq <abigailmiddleton2008@gmail.com>
2025-08-14 15:00:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById('sillyfish').addEventListener('click', function() {
 
 document.querySelector('.homebuttons').classList.add('fade-in');
 setTimeout(() => {
-    .homebuttons.classList.remove("hidden"); // show container
+    homebuttons.classList.remove("hidden"); // show container
     setTimeout(() => {
-      homeButtons.classList.add("visible"); // fade in
+      homeButtons.classList.add("fade-in"); // fade in
     }, 50); // tiny delay to trigger opacity transition
   }, 800); // matches .slide-up transition time
 });
