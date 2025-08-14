2526aa8d3e271429162678ce6f197068b9c267cd
eq <abigailmiddleton2008@gmail.com>
2025-08-14 11:45:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,4 @@ document.getElementById('sillyfish').addEventListener('click', function() {
   document.querySelector('.center-wrapper').classList.add('slide-up');
 });
 
-  // Wait until slide animation finishes
-  setTimeout(() => {
-    document.querySelector('.homebuttons').classList.remove("hidden"); // show container
-    setTimeout(() => {
-    document.querySelector('.homebuttons-visible').classList.add("visible"); // fade in
-    }, 50); // tiny delay to trigger opacity transition
-  }, 800); // matches .slide-up transition time
-});
-
   
