230f37a7ec77b9057bb9af97de81c733028c5eb0
eq <abigailmiddleton2008@gmail.com>
2025-08-14 09:30:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("sillyfish").addEventListener("click", function() {
   document.querySelector(".center-wrapper").classList.add("slide-up");
 });
-document.getElementById("
+document.getElementById(".homebuttons").addEventListener("
 document.querySelector(".homebuttons").classList.add("fade-in");
 });
 document.getElementById("about").addEventListener("click", function() {
