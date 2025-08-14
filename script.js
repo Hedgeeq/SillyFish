195b6524e7469f48f7c000bab613f30c15ac7f32
eq <abigailmiddleton2008@gmail.com>
2025-08-14 09:46:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const sillyfish = document.getElementById("sillyfish");
-const centerWrapper = document.querySelector(".center-wrapper");
-const homeButtons = document.querySelector(".homebuttons");
-
-sillyfish.addEventListener("click", () => {
-  centerWrapper.classList.add("center-wrapper.slide-up");
+document.getElementById('sillyfish').addEventListener('click', function() {
+  document.querySelector('.center-wrapper').classList.add('slide-up');
+});
 
   // Wait until slide animation finishes
   setTimeout(() => {
-    homeButtons.classList.remove("hidden"); // show container
+    document.querySelector('.homebuttons').classList.remove("hidden"); // show container
     setTimeout(() => {
-      homeButtons.classList.add("visible"); // fade in
+    document.querySelector('.homebuttons').classList.add("visible"); // fade in
     }, 50); // tiny delay to trigger opacity transition
   }, 800); // matches .slide-up transition time
 });
